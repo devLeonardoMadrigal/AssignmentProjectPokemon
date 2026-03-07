37b83e5397415d0fe351d94379148e1455fa6163
Leonardo Madrigal <devLeonardoMadrigal@gmail.com>
2026-03-07 10:50:33 -0500

    #3 previous and next properties were always expering a value, even when reaching the very first or the very last item. Fields are now nullable to avoid this error

--- app/src/main/java/com/aa/android/pokedex/api/entity/ResourceListDTO.kt
+++ app/src/main/java/com/aa/android/pokedex/api/entity/ResourceListDTO.kt
@@ -17,9 +17,9 @@ data class ResourceListDTO(
     @Json(name = "count")
     val count: Int,
     @Json(name = "next")
-    val next: String,
+    val next: String?,
     @Json(name = "previous")
-    val previous: String,
+    val previous: String?,
     @Json(name = "results")
     val results: List<ResourceDTO>
 )
