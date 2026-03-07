dd6a093060f6814bb13ee6b4527ee9478b8b79dd
Leonardo Madrigal <devLeonardoMadrigal@gmail.com>
2026-03-07 14:41:57 -0500

    Implemented getPokemon function in the PokemonRepository to fetch details from a pokemon using its name

--- app/src/main/java/com/aa/android/pokedex/repository/PokemonRepository.kt
+++ app/src/main/java/com/aa/android/pokedex/repository/PokemonRepository.kt
@@ -1,8 +1,10 @@
 package com.aa.android.pokedex.repository
 
 import com.aa.android.pokedex.api.PokemonApi
+import com.aa.android.pokedex.api.entity.PokemonDTO
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Inject
@@ -21,5 +23,17 @@ class PokemonRepository @Inject constructor(retrofit: Retrofit) {
             }
         }
         return listOf()
+    }
+
+    suspend fun getPokemon(name: String) : PokemonDTO {
+        val response = api.getPokemon(name)
+
+        if(response.isSuccessful){
+            response.body()?.let {
+                return it
+            }
+        }
+
+        throw Exception("Error fetching Pokemon")
     }
 }
