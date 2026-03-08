e8230b1b6542f3a9e45939ff2893240c2b0c3e4e
Leonardo Madrigal <devLeonardoMadrigal@gmail.com>
2026-03-07 21:17:22 -0500

    Implemented caching layer for HTTP calls

--- app/src/main/java/com/aa/android/pokedex/di/NetworkModule.kt
+++ app/src/main/java/com/aa/android/pokedex/di/NetworkModule.kt
@@ -1,17 +1,31 @@
 package com.aa.android.pokedex.di
 
+import android.content.Context
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(@ApplicationContext context: Context): OkHttpClient{
+        val cacheSize = (10 * 1024 * 1024).toLong()
+        return OkHttpClient.Builder()
+            .cache(Cache(context.cacheDir,cacheSize))
+            .build()
+    }
 
     @Provides
     fun provideBaseUrl(): String = "https://pokeapi.co/api/v2/"
@@ -20,9 +34,10 @@ object NetworkModule {
     fun provideMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     @Provides
-    fun provideRetrofit(baseUrl: String, moshi: Moshi): Retrofit {
+    fun provideRetrofit(baseUrl: String, moshi: Moshi, okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
