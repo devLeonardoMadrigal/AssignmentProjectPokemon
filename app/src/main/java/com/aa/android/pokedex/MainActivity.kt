8e1a5f438e8de9b79ced790407ce06e8c8fdd495
Leonardo Madrigal <devLeonardoMadrigal@gmail.com>
2026-03-07 19:00:48 -0500

    Added Icons dependency to gradle (:app)
    Implemented back button on DetailsScreen
    
    Added all other properties for pokemon details screen and iterated a better design

--- app/src/main/java/com/aa/android/pokedex/MainActivity.kt
+++ app/src/main/java/com/aa/android/pokedex/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.aa.android.pokedex
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,19 +18,24 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.aa.android.pokedex.api.entity.PokemonDTO
+import com.aa.android.pokedex.model.Type
 import com.aa.android.pokedex.model.UiState
 import com.aa.android.pokedex.ui.theme.PokedexTheme
 import com.aa.android.pokedex.viewmodel.MainViewModel
@@ -62,7 +72,8 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("PokemonDetails") {
                         PokemonDetailScreen(
-                            pokemonDetailsLiveData = detailViewModel.pokemonDetail
+                            pokemonDetailsLiveData = detailViewModel.pokemonDetail,
+                            onBackClick = { navController.navigateUp() }
                         )
                     }
 
@@ -151,28 +162,120 @@ fun DefaultPreview() {
     }
 }
 
+@SuppressLint("DefaultLocale")
 @Composable
 fun PokemonDetailScreen(
-    pokemonDetailsLiveData : LiveData<UiState<PokemonDTO>>
+    pokemonDetailsLiveData : LiveData<UiState<PokemonDTO>>,
+    onBackClick: () -> Unit
 ) {
     val uiState by pokemonDetailsLiveData.observeAsState(UiState.Loading())
 
-    uiState?.let {
-        when(it) {
-            is UiState.Error ->  {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    textAlign = TextAlign.Center,
-                    text = "Error loading Pokemon. Please try again later.",
-                    color = MaterialTheme.colors.onBackground
-                )
-            }
-            is UiState.Loading -> CircularProgressIndicator()
-            is UiState.Ready -> {
-                Text(it.data.name)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text("Details screen")},
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+        uiState.let {
+            when(it) {
+                is UiState.Error ->  {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        textAlign = TextAlign.Center,
+                        text = "Error loading Pokemon. Please try again later.",
+                        color = MaterialTheme.colors.onBackground
+                    )
+                }
+                is UiState.Loading -> CircularProgressIndicator()
+                is UiState.Ready -> {
 
+
+                        Text(
+                            it.data.name.capitalize(Locale.current),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 32.sp
+                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            AsyncImage(
+                                model = it.data.sprites.defaultFront,
+                                contentDescription = "Front sprite for ${it.data.name}",
+                                modifier = Modifier.size(200.dp)
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text("Height: ", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                            Text(
+                                "${String.format(format = "%.2f", it.data.height * 10 / 2.54)} ft",
+                                fontSize = 24.sp
+                            )
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text("Weight: ", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                            Text(
+                                "${String.format(format = "%.2f", it.data.weight / 10 * 2.20462)} lbs",
+                                fontSize = 24.sp
+                            )
+                        }
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text("Types: ")
+                            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+
+                                items(it.data.types) { currentType ->
+                                    val backgroundColor = try {
+                                        Type.getColor(currentType.type.name.uppercase())
+                                    } catch (e: IllegalArgumentException) {
+                                        Type.UNKNOWN.color
+                                    }
+                                    Text(
+                                        currentType.type.name,
+                                        modifier = Modifier.background(backgroundColor)
+                                    )
+                                }
+                            }
+                        }
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Text("Stats:")
+                            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                                items(it.data.stats) { currentStat ->
+                                    Row(Modifier.fillMaxWidth()) {
+                                        Text("${currentStat.stat.name}:")
+                                        Spacer(Modifier.size(12.dp))
+                                        Text(currentStat.baseStat.toString())
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
